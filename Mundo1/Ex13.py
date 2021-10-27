faa760089dbbe02a58a3ae201ac23ec929b6c869
Bruno Lima <brunosssss@gmail.com>
2021-10-27 17:54:13 -0300

    exercicio13

--- Mundo1/Ex13.py
+++ Mundo1/Ex13.py
@@ -3,4 +3,4 @@ salario = float(input("Digite seu Salario: "))
 salarioAumento = salario + (salario * 0.15)
 
 print(f"Ola {nome} voce teve um rejuste no seu salario antes era {salario:.2f} e com o reajuste \n"
-      f" de 15% foi para {salarioAumento:.2f}!!!!!!!! ")
+      f" de 15% foi para {salarioAumento:.2f}!!!!!!!!! ")
