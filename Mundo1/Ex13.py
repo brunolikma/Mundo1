19f2a93ff9468554d29bbdb4b92037f5d9b2f2da
Bruno Lima <brunosssss@gmail.com>
2021-10-27 17:48:02 -0300

    exercicio13 guanabara

---
+++ Mundo1/Ex13.py
@@ -0,0 +1,6 @@
+nome = input("Digite seu Nome:")
+salario = float(input("Digite seu Salario: "))
+salarioAumento = salario + (salario * 0.15)
+
+print(f"Ola {nome} voce teve um rejuste no seu salario antes era {salario:.2f} e com o reajuste \n"
+      f" de 15% foi para {salarioAumento:.2f}!!!!!!!! ")
