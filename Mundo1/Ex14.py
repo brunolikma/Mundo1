4d901902eb7f905cd5fe4b10166d57ba6bc7d6d4
Bruno Lima <brunosssss@gmail.com>
2021-10-28 00:41:09 -0300

    exercicio14

--- Mundo1/Ex14.py
+++ Mundo1/Ex14.py
@@ -1,4 +1,4 @@
 celsius = float(input("Digite o a termperatura: "))
 fahrenheit = (celsius * 9/5) + 32
 
-print(f"A temperatura em  {celsius:.1f} grau celsius equevale a {fahrenheit} Fahrenheit ")
+print(f"A temperatura em  {celsius:.1f} grau celsius equevale a {fahrenheit} Fahrenheit ! ")
