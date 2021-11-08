7f42be8940f9f831166b321f57a1414338fdbdd8
Bruno Lima <brunosssss@gmail.com>
2021-11-08 14:11:31 -0300

    qual é o maior e qual o menor

--- Mundo1/Exercicios Condição/Ex33.py
+++ Mundo1/Exercicios Condição/Ex33.py
@@ -2,5 +2,5 @@ n1 = int(input("Digite o Primeiro Numero: "))
 n2 = int(input("Digite o Segundo Numero: "))
 n3 = int(input("Digite o Terceiro Numero: "))
 lista = [n1, n2, n3]
-decrecenteLista = sorted(lista,reverse=True)
-print(f"O maior numero é {decrecenteLista[0]} e o menor numero é {decrecenteLista[len(decrecenteLista) - 1]}")
+ordemLista = sorted(lista)
+print(f"O maior numero é {ordemLista[2]} e o menor numero é {ordemLista[0]}")
