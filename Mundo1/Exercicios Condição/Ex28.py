8c2fe1328e63140fd203ad1e98fe5537e2ed5b4d
Bruno Lima <brunosssss@gmail.com>
2021-11-08 07:45:47 -0300

    jogo de adivinhacao de numero

---
+++ Mundo1/Exercicios Condição/Ex28.py
@@ -0,0 +1,7 @@
+from random import randint
+n1 = randint(0,5)
+chute = int(input("Faca seu chute: "))
+if n1 == chute:
+    print("Voce venceu!!!!!!!!!!!!")
+else:
+    print("Voce Perdeu !!!!!!!!!!!")
